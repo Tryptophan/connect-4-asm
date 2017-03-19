a0c6ff93e4477bc408756e5c0dbde23cb55c6378
Garrett Battaglia <garrett@garrettbattaglia.info>
2017-03-19 15:45:19 -0500

    check downward also changed jal/jr with call/return macros

--- main.asm
+++ main.asm
@@ -1,3 +1,25 @@
+
+.macro push (%reg)
+	addi $sp, $sp, -4
+	sw %reg, 0($sp)
+.end_macro 
+
+.macro pop (%reg)
+	lw %reg, ($sp)
+	addi $sp, $sp, 4
+.end_macro
+
+.macro call (%label)
+	push $ra
+	jal %label
+.end_macro
+
+.macro return
+	move $t0, $ra
+	pop $ra
+	jr $t0
+.end_macro
+
 .data 
 #board is a 2D array of 1 byte entrys
 boardStart: 
@@ -19,17 +41,17 @@ winMsg:
 	
 .text
 setup:
-	jal prompts
-	jal drawBoard
+	call prompts
+	call drawBoard
 	bnez  $s1, compGameLoop
 	
 gamePlayLoop:
 	#TODO add tile t0
 	li $a2, 0
-	j winCheck #BUG other team still plays if t0 wins
+	call winCheck #BUG other team still plays if t0 wins
 	#TODO add tile t1
 	li $a2, 1
-	j winCheck 
+	call winCheck 
 	bnez $s0, win #met win condition, BUG: if both teams win on the same turn t1 always wins, 
 	j gamePlayLoop
 	#you should never get here
@@ -38,10 +60,10 @@ gamePlayLoop:
 compGameLoop:
 	#TODO add tile t0
 	li $a2, 0
-	j winCheck #BUG other team still plays if t0 wins
+	call winCheck #BUG other team still plays if t0 wins
 	#TODO add tile t1
 	li $a2, 1
-	j winCheck 
+	call winCheck 
 	bnez $s0, win #met win condition
 	j compGameLoop 
 	#should never get here
@@ -51,7 +73,7 @@ drawBoard:
 	la $a0, board # load addr of board into reg a0
 	li $v0, 4 # syscall # for "print string"
 	syscall
-	jr $31 #ret
+	return #ret
 	
 #in: $a0 = row, $a1 = col  
 #ret: $v0 = square's value
@@ -59,13 +81,13 @@ getSquare:
 	sll $t0, $a0, 3
 	add $t0,$t0,$a1
 	lbu $v0,boardStart($t0)
-	jr $31 #ret
+	return#ret
 	
 prompts:#TODO
 	la $a0, playerPrompt # load addr of playerPrompt into reg a0
 	li $v0, 4 # syscall # for "print string"
 	syscall
-	jr $31 #ret
+	return #ret
 	
 win:
 #TODO print win message
@@ -76,34 +98,34 @@ win:
 getOcc:
 	andi $t0, $a0, 0x80 # MS bit is if cell is occ
 	sltiu $v0, $t1,1
-	jr $ra
+	return
 	
 # in: $a0 = cell content from array
 # ret: $v0 = team
 getTeam:
 	andi $t0, $a0, 0x40 # 7th MS bit is team
 	sltiu $v0, $t1,1
-	jr $ra
+	return
 
 # in: $a0 = cell content from array
 # ret: $v0 = run length
 getRunLength:
 	andi $t0, $a0, 0x30 # 5 and 6th MS bit is run length
 	srl $v0, $a0, 4 #move to begining
-	jr $ra
+	return
 	
 # in: $a0 = cell content from array
 # ret: $v0 = run dir
 getRunDir:
 	andi $t0, $a0, 0xE #bit 1-3 is dir
 	srl $v0, $t0, 1
-	jr $ra
+	return
 	
 # in: $a0 = cell content from array
 # ret: $v0 = 1 if multiple intersecting runs
 getMultiRun:
 	andi $v0, $a0, 1
-	jr $ra
+	return
 	
 #in: $a0= row, $a1 = col, $a2 =  team
 #out: $s0 = win
@@ -116,16 +138,25 @@ winCheck:
 #in: $a0= row, $a1 = col, $a2 =  team
 #out: $s0 = win	
 winCheckBottom:
-beq $a1, 0, winCheckBottomRight
-beq $a1, 6, winCheckBottomLeft
+	beq $a1, 0, winCheckBottomRight
+	beq $a1, 6, winCheckBottomLeft
 
 #in: $a0= row, $a1 = col, $a2 =  team
 #out: $s0 = win
 winCheckRightSide:
-jal getSquare
-move $a0, $s2
-move $a1, $s3
-move $v0, $a0
+	move $a0, $s2
+	move $a1, $s3
+	call checkBelow
+
+checkBelow:
+	subi $a0, $s2 ,1
+	move $a1, $s3
+	call getSquare
+	move $v0, $a0
+	call getTeam
+	li $a3, 5
+	beq $v0,$a2, calcRun
+	return
 
 
 #in: $a0= row, $a1 = col, $a2 =  team
@@ -154,7 +185,7 @@ gravity:
 	lbu $v0, topStart($a0) #load next aval cell
 	addiu $t0,$v0,1 #move up one
 	sw $t0, topStart($a0) #store it back
-	jr $ra #ret
+	return #ret
 
 	
 exit:
