ae8605dfde411c728ffdd13c4d0c5d051b578238
LoadingPleaseWait <michael@murphey.org>
2017-04-23 21:34:11 -0500

    Place pieces and print bytes in board

--- main.asm
+++ main.asm
@@ -23,15 +23,17 @@
 .data 
 #board is a 2D array of 1 byte entrys
 boardStart: 
-	.space 48 
+	.space 42 
 topStart:
-	.space 7
+	.space 6
 board:
 	.asciiz "" #TODO
 playerPrompt: 
 	.asciiz "" #TODO
 winMsg:
 	.asciiz "" #TODO
+newLine:
+	.asciiz "\n"
 
 	#$s0 is always win condition, 10 for t1	  , 11 for t2
 	#$s1 is the type of game, 0 for 2 player, 1 for computer 
@@ -42,14 +44,23 @@ winMsg:
 .text
 setup:
 	call prompts
+	li $a0, 1
+	li $a1, 1
+	call placePiece
+	call drawBoard
+	li $a0, 1
+	li $a1, 1
+	call placePiece
 	call drawBoard
 	bnez  $s1, compGameLoop
 	
 gamePlayLoop:
 	#TODO add tile t0
+	# check if player 0 has won
 	li $a2, 0
 	call winCheck #BUG other team still plays if t0 wins
 	#TODO add tile t1
+	# check if player 1 has won
 	li $a2, 1
 	call winCheck 
 	bnez $s0, win #met win condition, BUG: if both teams win on the same turn t1 always wins, 
@@ -69,7 +80,43 @@ compGameLoop:
 	#should never get here
 	j exit
 	
+# $a0: column
+# $a1: player number
+placePiece:
+	call gravity
+	move $t2, $a1
+	move $a1, $v0
+	call boardIndex
+	sb $t2, boardStart($v0)
+	return
+	
 drawBoard:
+	# print bytes of the board
+	li $t0, 5 # $t0: outer loop iterator (row)
+	li $t1, -1 # $t1: outer loop number of iterations
+	forLoop:
+	
+	li $t2, 0 # $t2: inner loop iterator (column)
+	li $t3, 7 # $t3: inner loop number of iterations
+	innerForLoop:
+
+	# $t4 will be the index of the byte we're printing
+	mul $t4, $t0, $t3
+	add $t4, $t4, $t2
+	lbu $a0, boardStart($t4)
+	li $v0, 1
+	syscall
+
+	addi $t2, $t2, 1
+	blt $t2, $t3, innerForLoop
+
+	la $a0, newLine
+	li $v0, 4
+	syscall
+
+	addi $t0, $t0, -1
+	bgt $t0, $t1, forLoop
+
 	la $a0, board # load addr of board into reg a0
 	li $v0, 4 # syscall # for "print string"
 	syscall
@@ -96,7 +143,7 @@ win:
 # in: $a0 = cell content from array
 # ret: $v0 = 1 = occ, 0 = not occ
 getOcc:
-	andi $t0, $a0, 0x80 # MS bit is if cell is occ
+	andi $t0, $a0, 0x80 # MS (most significant) bit is if cell is occ(upied)
 	sltiu $v0, $t1,1
 	return
 	
@@ -155,7 +202,8 @@ checkBelow:
 	move $v0, $a0
 	call getTeam
 	li $a3, 5
-	beq $v0,$a2, calcRun
+	break
+	#beq $v0,$a2, calcRun
 	return
 
 
@@ -179,14 +227,22 @@ winCheckBottomLeft:
 winCheckMiddle:
 
 
-#in: $a0 = row
-#out: $v0 = col
+#in: $a0 = col
+#out: $v0 = row
 gravity:
 	lbu $v0, topStart($a0) #load next aval cell
 	addiu $t0,$v0,1 #move up one
-	sw $t0, topStart($a0) #store it back
+	sb $t0, topStart($a0) #store it back
 	return #ret
 
+# $a0 = col
+# $a1 = row
+# $v0 = position in byte array for the board
+boardIndex:
+	li $t3, 7
+	mul $v0, $a1, $t3
+	add $v0, $v0, $a0
+	return
 	
 exit:
 	li $v0, 10
